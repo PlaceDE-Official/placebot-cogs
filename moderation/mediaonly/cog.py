d7e27e4958fff3f72b7d32cb60cc9d62941304f8
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2024-03-22 01:24:50 +0100

    updated media_only

--- moderation/mediaonly/cog.py
+++ moderation/mediaonly/cog.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import Optional
 
 from aiohttp import ClientError, ClientSession
-from discord import Embed, Forbidden, Guild, Message, TextChannel
+from discord import Embed, Forbidden, Guild, Message
 from discord.ext import commands
 from discord.ext.commands import CommandError, Context, UserInputError, guild_only
 
@@ -11,8 +11,10 @@ from PyDrocsid.cog import Cog
 from PyDrocsid.command import docs, reply
 from PyDrocsid.database import db, filter_by
 from PyDrocsid.embeds import send_long_embed
+from PyDrocsid.emojis import emoji_to_name
 from PyDrocsid.events import StopEventHandling
 from PyDrocsid.translations import t
+from PyDrocsid.types import GuildMessageable
 
 from .colors import Colors
 from .models import MediaOnlyChannel, MediaOnlyDeletion
@@ -23,26 +25,52 @@ from ...pubsub import can_respond_on_reaction, get_userlog_entries, send_alert, 
 
 tg = t.g
 t = t.mediaonly
+emojis = list(sorted(set(map(re.escape, emoji_to_name)), key=lambda x: len(x), reverse=True))
+not_rendering_markdown_chars = ["```", "``", "`", ("<", ">")]
 
 
-async def contains_image(message: Message) -> bool:
-    urls = [(att.url,) for att in message.attachments]
-    urls += re.findall(r"(https?://([a-zA-Z0-9\-_~]+\.)+[a-zA-Z0-9\-_~]+(/\S*)?)", message.content)
-    for url, *_ in urls:
+def create_regex(chars, string) -> str:
+    if isinstance(chars, tuple):
+        return "(" + re.escape(chars[0]) + "[\\s\\S]*" + re.escape(string) + "[\\s\\S]*" + re.escape(chars[1]) + ")"
+    return "(" + re.escape(chars) + "[\\s\\S]*" + re.escape(string) + "[\\s\\S]*" + re.escape(chars) + ")"
+
+
+async def find_images(message: Message) -> list[str]:
+    urls = [att.url for att in message.attachments]
+    for link in re.finditer(r"(https?://([a-zA-Z0-9\-_~]+\.)+[a-zA-Z0-9\-_~]+(/\S*)?)", message.content):
+        found = True
+        for chars in not_rendering_markdown_chars:
+            for match in re.finditer(create_regex(chars, link.group(1)), message.content):
+                if match.start() <= link.start() and link.end() <= match.end():
+                    found = False
+                    break
+        if found:
+            urls.append(link.group(1))
+
+    out = []
+    for url in urls:
+        # tenor only sends html, but discord displays it as gif
+        if url.startswith("https://tenor.com/view/"):
+            out.append(url)
+            continue
         try:
             async with ClientSession() as session, session.head(url, allow_redirects=True) as response:
-                content_length = int(response.headers["Content-length"])
-                mime = response.headers["Content-type"]
-        except (KeyError, AttributeError, UnicodeError, ConnectionError, ClientError):
-            break
+                content_length = int(response.headers.get("Content-length") or 256)
+                mime = response.headers.get("Content-type") or ""
+        except (KeyError, AttributeError, UnicodeError, ConnectionError, ClientError) as e:
+            continue
+        if (
+            mime.lower() in ["image/gif", "image/gifv", "image/png", "image/jpg", "image/jpeg"]
+            and content_length >= 256
+        ):
+            out.append(url)
 
-        if mime.startswith("image/") and content_length >= 256:
-            return True
+    for sticker in message.stickers:
+        out.append(sticker.url)
+    return out
 
-    return False
 
-
-async def delete_message(message: Message):
+async def delete_message(message: Message, log: bool, mode: int, forbidden: bool, limit: int):
     try:
         await message.delete()
     except Forbidden:
@@ -50,15 +78,34 @@ async def delete_message(message: Message):
     else:
         deleted = True
 
-    await MediaOnlyDeletion.create(message.author.id, str(message.author), message.channel.id)
+    if log:
+        await MediaOnlyDeletion.create(message.author.id, str(message.author), message.channel.id)
 
-    embed = Embed(title=t.mediaonly, description=t.deleted_nomedia, colour=Colors.error)
-    await message.channel.send(content=message.author.mention, embed=embed, delete_after=30)
+        description = t.deleted.character_limit(limit)
+        if forbidden:
+            description = t.deleted[mode]
+        embed = Embed(title=t.mediaonly, description=description, colour=Colors.error)
+        await message.channel.send(content=message.author.mention, embed=embed, delete_after=30)
 
-    if deleted:
-        await send_alert(message.guild, t.log_deleted_nomedia(message.author.mention, message.channel.mention))
-    else:
-        await send_alert(message.guild, t.log_nomedia_not_deleted(message.author.mention, message.channel.mention))
+        if deleted:
+            await send_alert(message.guild, t.log_deleted_nomedia(message.author.mention, message.channel.mention))
+        else:
+            await send_alert(message.guild, t.log_nomedia_not_deleted(message.author.mention, message.channel.mention))
+
+
+async def split_message(message: Message) -> [int, int, int]:
+    urls = await find_images(message)
+    remaining_text = message.content
+    for url in urls:
+        remaining_text = remaining_text.replace(url, "")
+    emote_count = 0
+    for emoji in emojis:
+        remaining_text, count = re.subn(emoji, "", remaining_text, re.UNICODE)
+        emote_count += count
+    remaining_text, count = re.subn("<a?:.+?:\d+?>", "", remaining_text, re.UNICODE)
+    emote_count += count
+    remaining_text = remaining_text.strip()
+    return len(urls), emote_count, len(remaining_text)
 
 
 async def check_message(message: Message):
@@ -66,20 +113,37 @@ async def check_message(message: Message):
         return
     if await MediaOnlyPermission.bypass.check_permissions(message.author):
         return
-    if not await MediaOnlyChannel.exists(message.channel.id):
-        return
-    if await contains_image(message):
+    if not (media_only := await MediaOnlyChannel.get(message.channel.id))[0]:
         return
 
-    await delete_message(message)
-    raise StopEventHandling
+    mode = media_only[1]
+    max_length = int(media_only[2])
+    log = bool(media_only[3])
+
+    images, emotes, text = await split_message(message)
+
+    # store mode id, if max_length is exceeded; if forbidden content
+    for m, f_limit, f_forbidden in [
+        (1, lambda: 0 < max_length < images, lambda: emotes > 0 or text > 0),
+        (2, lambda: 0 < max_length < emotes, lambda: images > 0 or text > 0),
+        (4, lambda: 0 < max_length < text, lambda: images > 0 or emotes > 0),
+        (3, lambda: 0 < max_length < emotes + images, lambda: text > 0),
+        (5, lambda: 0 < max_length < text + images, lambda: emotes > 0),
+        (6, lambda: 0 < max_length < text + emotes, lambda: images > 0),
+        (7, lambda: 0 < max_length < text + emotes + images, lambda: False),
+    ]:
+        if mode == m:
+            fo = f_forbidden()
+            if f_limit() or fo:
+                await delete_message(message, log, mode, fo, max_length)
+                raise StopEventHandling
 
 
 class MediaOnlyCog(Cog, name="MediaOnly"):
     CONTRIBUTORS = [Contributor.Defelo, Contributor.wolflu]
 
     @can_respond_on_reaction.subscribe
-    async def handle_can_respond_on_reaction(self, channel: TextChannel) -> bool:
+    async def handle_can_respond_on_reaction(self, channel: GuildMessageable) -> bool:
         return not await db.exists(filter_by(MediaOnlyChannel, channel=channel.id))
 
     @get_userlog_entries.subscribe
@@ -111,15 +175,22 @@ class MediaOnlyCog(Cog, name="MediaOnly"):
         guild: Guild = ctx.guild
         out = []
         async for channel in MediaOnlyChannel.stream():
-            text_channel: Optional[TextChannel] = guild.get_channel(channel)
+            text_channel: Optional[GuildMessageable] = guild.get_channel(channel.channel)
             if not text_channel:
-                await MediaOnlyChannel.remove(channel)
+                await MediaOnlyChannel.remove(channel.channel)
                 continue
 
-            out.append(f":small_orange_diamond: {text_channel.mention}")
+            out.append(
+                f":small_orange_diamond: {text_channel.mention};`{channel.mode}`;`{channel.max_length}`;`{channel.log}`"
+            )
 
         embed = Embed(title=t.media_only_channels_header, colour=Colors.error)
         if out:
+            out.append("")
+            out.append("")
+            out.append(t.syntax_explanation)
+            out.append(t.mode_explanation)
+
             embed.colour = Colors.MediaOnly
             embed.description = "\n".join(out)
             await send_long_embed(ctx, embed)
@@ -129,23 +200,30 @@ class MediaOnlyCog(Cog, name="MediaOnly"):
 
     @mediaonly.command(name="add", aliases=["a", "+"])
     @MediaOnlyPermission.write.check
-    @docs(t.commands.add)
-    async def mediaonly_add(self, ctx: Context, channel: TextChannel):
-        if await MediaOnlyChannel.exists(channel.id):
+    @docs(f"{t.commands.add}\n{t.mode_explanation}")
+    async def mediaonly_add(
+        self, ctx: Context, channel: GuildMessageable, mode: int, max_length: int | None, log: bool = False
+    ):
+        if not 1 <= mode <= 7:
+            raise CommandError(t.mode_invalid)
+        if max_length < 0:
+            raise CommandError(t.max_length_negative)
+
+        if (await MediaOnlyChannel.get(channel.id))[0]:
             raise CommandError(t.channel_already_media_only)
         if not channel.permissions_for(channel.guild.me).manage_messages:
             raise CommandError(t.media_only_not_changed_no_permissions)
 
-        await MediaOnlyChannel.add(channel.id)
+        await MediaOnlyChannel.add(channel.id, mode, max_length, log)
         embed = Embed(title=t.media_only_channels_header, description=t.channel_now_media_only, colour=Colors.MediaOnly)
         await reply(ctx, embed=embed)
-        await send_to_changelog(ctx.guild, t.log_channel_now_media_only(channel.mention))
+        await send_to_changelog(ctx.guild, t.log_channel_now_media_only(channel.mention, mode, max_length, log))
 
     @mediaonly.command(name="remove", aliases=["del", "r", "d", "-"])
     @MediaOnlyPermission.write.check
     @docs(t.commands.remove)
-    async def mediaonly_remove(self, ctx: Context, channel: TextChannel):
-        if not await MediaOnlyChannel.exists(channel.id):
+    async def mediaonly_remove(self, ctx: Context, channel: GuildMessageable):
+        if not (await MediaOnlyChannel.get(channel.id))[0]:
             raise CommandError(t.channel_not_media_only)
 
         await MediaOnlyChannel.remove(channel.id)
--- moderation/mediaonly/models.py
+++ moderation/mediaonly/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from typing import AsyncIterable, Union
 
 from discord.utils import utcnow
-from sqlalchemy import BigInteger, Column, Integer, Text
+from sqlalchemy import BigInteger, Boolean, Column, Integer, Text
 
 from PyDrocsid.database import Base, UTCDateTime, db, delete, filter_by, select
 from PyDrocsid.environment import CACHE_TTL
@@ -15,34 +15,41 @@ class MediaOnlyChannel(Base):
     __tablename__ = "mediaonly_channel"
 
     channel: Union[Column, int] = Column(BigInteger, primary_key=True, unique=True)
+    mode: Union[Column, int] = Column(BigInteger, default=1, nullable=False)
+    max_length: Union[Column, int] = Column(BigInteger, default=0)
+    log: Union[Column, bool] = Column(Boolean, default=False, nullable=False)
 
     @staticmethod
-    async def add(channel: int):
-        await redis.setex(f"mediaonly:channel={channel}", CACHE_TTL, 1)
-        await db.add(MediaOnlyChannel(channel=channel))
+    async def add(channel: int, mode: int, max_length: int, log: bool):
+        await redis.setex(f"mediaonly:channel={channel}", CACHE_TTL, f"1;{mode};{max_length};{int(log)}")
+        await db.add(MediaOnlyChannel(channel=channel, mode=mode, max_length=max_length, log=log))
 
     @staticmethod
-    async def exists(channel: int) -> bool:
+    async def get(channel: int) -> tuple[bool, int, int, bool]:
         if result := await redis.get(key := f"mediaonly:channel={channel}"):
-            return result == "1"
+            split = result.split(";")
+            return split[0] == "1", int(split[1]), int(split[2]), bool(int(split[3]))
 
-        result = await db.exists(filter_by(MediaOnlyChannel, channel=channel))
-        await redis.setex(key, CACHE_TTL, int(result))
-        return result
+        if result := await db.first(filter_by(MediaOnlyChannel, channel=channel)):
+            await redis.setex(key, CACHE_TTL, f"1;{result.mode};{result.max_length};{int(result.log)}")
+            return True, int(result.mode), int(result.max_length), bool(result.log)
+        await redis.setex(f"mediaonly:channel={channel}", CACHE_TTL, "0;0;0;0")
+        return False, 0, 0, False
 
     @staticmethod
-    async def stream() -> AsyncIterable[int]:
+    async def stream() -> AsyncIterable[MediaOnlyChannel]:
         row: MediaOnlyChannel
         async with redis.pipeline() as pipe:
             async for row in await db.stream(select(MediaOnlyChannel)):
-                channel = row.channel
-                await pipe.setex(f"mediaonly:channel={channel}", CACHE_TTL, 1)
-                yield channel
+                await pipe.setex(
+                    f"mediaonly:channel={row.channel}", CACHE_TTL, f"1;{row.mode};{row.max_length};{int(row.log)}"
+                )
+                yield row
             await pipe.execute()
 
     @staticmethod
     async def remove(channel: int):
-        await redis.setex(f"mediaonly:channel={channel}", CACHE_TTL, 0)
+        await redis.delete(f"mediaonly:channel={channel}")
         await db.exec(delete(MediaOnlyChannel).filter_by(channel=channel))
 
 
